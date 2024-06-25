3db88aee9effbe008d1521f394fc74c6f48b67d3
dianprsty <dprasmail@gmail.com>
2024-06-25 15:53:20 +0700

    setup recipe page project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 function App() {
   return (
     <>
-      <div className="w-screen h-screen flex justify-center items-center bg-blue-200">
-        <h1 className="text-xl sm:text-2xl md:text-4xl lg:text-6xl font-bold text-center text-blue-950">
-          React JS + Typecript + Tailwind CSS Project Starter
+      <div className="flex items-center justify-center w-screen h-screen bg-blue-200">
+        <h1 className="text-xl font-bold text-center sm:text-2xl md:text-4xl lg:text-6xl text-blue-950">
+          Recipe Page
         </h1>
       </div>
+      <footer className="p-4">
+        <div className="attribution">
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          <br /> Coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/dianprasetyo-2021/"
+            target="_blank"
+          >
+            Dian Prasetyo
+          </a>
+        </div>
+      </footer>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mobile: "375px",
+        desktop: "1440px",
+      },
+      colors: {
+        nutmeg: "hsl(14, 45%, 36%)",
+        "dark-raspberry": " hsl(332, 51%, 32%)",
+        "rose-white": "hsl(330, 100%, 98%)",
+        eggshell: "hsl(30, 54%, 90%)",
+        "light-grey": "hsl(30, 18%, 87%)",
+        "wenge-brown": "hsl(30, 10%, 34%)",
+        "dark-charcoal": "hsl(24, 5%, 18%)",
+      },
+      fontFamily: {
+        "young-serif": ["Young Serif", "sans-serif"],
+        outfit: ["Outfit", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
